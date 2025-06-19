7d4c374c38abc658c70f9b2929d6cb0a3bc44216
Hao Mi <mihao_2002@hotmail.com>
2025-06-18 21:20:56 -0700

    init commit

---
+++ MultiAngleTextureMapping/MultiAngleTextureMappingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiAngleTextureMappingApp.swift
+//  MultiAngleTextureMapping
+//
+//  Created by Hao Mi on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MultiAngleTextureMappingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MultiAngleTextureMappingTests/MultiAngleTextureMappingTests.swift
@@ -0,0 +1,17 @@
+//
+//  MultiAngleTextureMappingTests.swift
+//  MultiAngleTextureMappingTests
+//
+//  Created by Hao Mi on 6/18/25.
+//
+
+import Testing
+@testable import MultiAngleTextureMapping
+
+struct MultiAngleTextureMappingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
